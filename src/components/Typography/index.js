9d9d1fea92ea52b791ca21003cfa471df00cf802
Bastien Robert <bastienrobert@outlook.com>
2018-12-14 16:55:02 +0100

    Case sensitive commit

---
+++ src/components/Typography/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import classnames from 'classnames/bind'
+
+import css from './styles.scss'
+
+const cx = classnames.bind(css)
+
+export default class Typography extends Component {
+  render() {
+    const childrenStyle = cx(css.Typography, this.props.className, {
+      title: this.props.type === 'title',
+      subtitle: this.props.type === 'subtitle',
+      text: this.props.type === 'text'
+    })
+
+    return React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: cx(child.props.className, childrenStyle)
+      })
+    })
+  }
+}
