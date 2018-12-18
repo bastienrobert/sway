b7fb48d0e44e985f10dc7aba5b779f3d12c65b6a
HARRIBEY Mathieu <mharribey@g-a102b-06.local>
2018-12-18 11:32:26 +0100

    move css properties

--- src/components/Art/Sabine/index.js
+++ src/components/Art/Sabine/index.js
@@ -9,21 +9,20 @@ export default class Sabine extends Oeuvre {
   timelines = timelines
 
   render() {
-    this.references.stormClouds = {}
 
     return (
       <div className={css.Sabine} ref="component">
         <div ref="cube" className={css.cube} />
         <div className={css.stormClouds}>
-          <div className={css.stromCloudBrushSmallRight} ref={el => el && (this.references.stormClouds.stromCloudBrushSmallRight = el)}></div>
-          <div className={css.stromCloudPointSmall} ref={el => el && (this.references.stormClouds.stromCloudPointSmall = el)}></div>
-          <div className={css.stromCloudOrange} ref={el => el && (this.references.stormClouds.stromCloudOrange = el)}></div>
-          <div className={css.stromCloudBrushSmallLeft} ref={el => el && (this.references.stormClouds.stromCloudBrushSmallLeft = el)}></div>
-          <div className={css.stromCloudPointBig} ref={el => el && (this.references.stormClouds.stromCloudPointBig = el)}></div>
-          <div className={css.stromCloudBrushBigRight} ref={el => el && (this.references.stormClouds.stromCloudBrushBigRight = el)}></div>
-          <div className={css.stromCloudBrushBigLeft} ref={el => el && (this.references.stormClouds.stromCloudBrushBigLeft = el)}></div>
-          <div className={css.stromCloudGrey} ref={el => el && (this.references.stormClouds.stromCloudGrey = el)}></div>
-          <div className={css.stromCloudBlack} ref={el => el && (this.references.stormClouds.stromCloudBlack = el)}></div>
+          <div className={css.stromCloudBrushSmallRight} ref="stromCloudBrushSmallRight"></div>
+          <div className={css.stromCloudPointSmall} ref="stromCloudPointSmall"></div>
+          <div className={css.stromCloudOrange} ref="stromCloudOrange"></div>
+          <div className={css.stromCloudBrushSmallLeft} ref="stromCloudBrushSmallLeft"></div>
+          <div className={css.stromCloudPointBig} ref="stromCloudPointBig"></div>
+          <div className={css.stromCloudBrushBigRight} ref="stromCloudBrushBigRight"></div>
+          <div className={css.stromCloudBrushBigLeft} ref="stromCloudBrushBigLeft"></div>
+          <div className={css.stromCloudGrey} ref="stromCloudGrey"></div>
+          <div className={css.stromCloudBlack} ref="stromCloudBlack"></div>
         </div>
       </div>
     )
